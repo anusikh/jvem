66ac7ad6b86b93727ddd2c29e4e5cbf9dd3eb24e
Priyansh Choudhary <im1706@gmail.com>
2024-03-07 01:41:05 +0530

    Added constants map

--- src/utils/env_ops.rs
+++ src/utils/env_ops.rs
@@ -1,28 +1,22 @@
-use std::error::Error;
+use crate::constants::versions::constants;
 
 pub fn read_versions() -> Result<(), Box<dyn std::error::Error>> {
-    // Load environment variables from .env file
-    dotenv::from_path("./.env").ok();
+    let available_versions = constants::AVAILABLE_VERSIONS;
+    let versions: Vec<&str> = available_versions.split(',').collect();
 
-    if let Ok(available_versions) = std::env::var("AVAILABLE_VERSIONS") {
-        let versions: Vec<&str> = available_versions.split(',').collect();
-
-        for version in versions {
-            println!("{}", version);
-        }
+    println!("Availaible Versions:");
+    for version in versions {
+        println!("{}", version);
     }
 
     Ok(())
 }
 
-pub fn get_download_link(name: String, os: &str) -> Result<String, Box<dyn Error>> {
-    // Load environment variables from .env file
-    dotenv::from_path("./.env").ok();
-
-    // Access the environment variable for the specified name
+pub fn get_download_link(name: String, os: &str) -> Result<String, Box<dyn std::error::Error>> {
     let env_var_name = format!("{}_{}", name.to_uppercase(), os.to_uppercase());
-    match std::env::var(&env_var_name) {
-        Ok(value) => Ok(value),
-        Err(_) => Err("Couldn't recognize OS or the specified JDK is not available".into()),
+    if let Some(value) = constants::get_constant(&env_var_name) {
+        Ok(value.to_string())
+    } else {
+        Err("Couldn't recognize OS or the specified JDK is not available".into())
     }
 }
