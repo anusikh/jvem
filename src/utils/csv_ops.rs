1878ce02796d34987dc5d557ae52a294c15644be
Priyansh Choudhary <im1706@gmail.com>
2024-03-06 15:46:49 +0530

    Updated cargo.lock

--- src/utils/csv_ops.rs
+++ src/utils/csv_ops.rs
@@ -1,10 +1,10 @@
 use std::error::Error;
 
-fn read_versions() -> Result<(), Box<dyn std::error::Error>> {
+pub fn read_versions() -> Result<(), Box<dyn std::error::Error>> {
     // Load environment variables from .env file
     dotenv::dotenv().ok();
 
-    if let Ok(available_versions) = env::var("AVAILABLE_VERSIONS") {
+    if let Ok(available_versions) = std::env::var("AVAILABLE_VERSIONS") {
         let versions: Vec<&str> = available_versions.split(',').collect();
 
         for version in versions {
@@ -23,7 +23,7 @@ pub fn get_download_link(name: String, os: &str) -> Result<String, Box<dyn Error
 
     // Access the environment variable for the specified name
     let env_var_name = format!("{}_{}", name, os.to_uppercase());
-    match env::var(&env_var_name) {
+    match std::env::var(&env_var_name) {
         Ok(value) => Ok(value),
         Err(_) => Err("Couldn't recognize OS or the specified JDK is not available".into()),
     }
